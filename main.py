0af2b10b49c8a0d92af97904f3b9483847963d37
Nika <60982999+nosopirka@users.noreply.github.com>
2021-04-26 06:57:29 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,117 @@
+from flask import Flask, render_template, request, make_response, session, redirect, abort
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from data import db_session
+import sqlite3
+from data.users import User
+from forms.LoginForm import LoginForm
+from forms.user import RegisterForm
+from data import db_session
+from flask import make_response, jsonify
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+def main():
+    db_session.global_init("db/articles.db")
+    app.run()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.route("/")
+def index():
+    return render_template("base.html", title="Главная")
+
+
+@app.route('/algebra',  methods=['GET', 'POST'])
+def algebra():
+    return render_template('algebra.html', title='Алгебра')
+
+
+@app.route('/geometry',  methods=['GET', 'POST'])
+def geometry():
+    return render_template('geometry.html', title='Геометрия')
+
+
+@app.route('/informatics',  methods=['GET', 'POST'])
+def informatics():
+    return render_template('informatics.html', title='Информатика')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/connection')
+def connection():
+    return render_template("donation.html", title="Связь")
+
+
+@app.route('/user')
+def user():
+    return render_template("user.html", title="Пользователь", user=current_user)
+
+
+@app.route('/bot')
+def bot():
+    return render_template("bot.html", title="Бот")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+if __name__ == '__main__':
+    main()
